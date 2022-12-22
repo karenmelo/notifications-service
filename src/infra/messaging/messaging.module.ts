d1f74acc58fe49a676f02d56bcd81ac694e92756
Karen Melo <karen_melo@live.com>
2022-12-22 01:27:10 -0300

    Modificacoes, inclusoes do payload para recebimento de mensagem pelo consumer para salvar no banco.

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
+import { DatabaseModule } from './../database/database.module';
+import { SendNotification } from './../../application/use-cases/send-notification';
 import { NotificationsController } from './kafka/controllers/notifications.controllers';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
-  providers: [KafkaConsumerService],
+  imports: [DatabaseModule],
+  providers: [KafkaConsumerService, SendNotification],
   controllers: [NotificationsController],
 })
 export class MessagingModule {}
